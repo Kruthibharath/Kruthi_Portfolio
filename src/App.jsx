a7cb497ea7023d5125d93413e0551cc3f8a9adb9
Kruthibharath <kruthi.bharath@gmail.com>
2024-02-08 22:14:15 +0000

    chore:gh-page host

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { Route, Routes } from "react-router-dom";
 import { AboutMe } from "./pages/AboutMe/AboutMe";
 import { Home } from "./pages/Home/Home";
@@ -8,14 +8,14 @@ import { Contact } from "./pages/Contact/Contact";
 function App() {
   return (
     <>
-      <HashRouter>
+      <BrowserRouter basename="/Kruthi_Portfolio/">
         <Routes>
           <Route index element={<Home />} />
           <Route path="aboutMe" element={<AboutMe />} />
           <Route path="experience" element={<Experience />} />
           <Route path="contact" element={<Contact />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import { resolve } from "path";
 export default defineConfig({
   base: "/Kruthi_Portfolio/",
   plugins: [react()],
+
   build: {
     outDir: "dist", // Change 'dist' to your desired output directory
   },
