86502e527fd2573700a6ba4a78fba2cd54873938
Kruthibharath <kruthi.bharath@gmail.com>
2024-02-08 17:14:54 +0000

    revert:changed absolute path to relative path

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { HashRouter } from "react-router-dom";
 import { Route, Routes } from "react-router-dom";
-import { AboutMe } from "/pages/AboutMe/AboutMe";
-import { Home } from "/pages/Home/Home";
-import { Experience } from "/pages/Experience/Experience";
-import { Contact } from "/pages/Contact/Contact";
+import { AboutMe } from "./pages/AboutMe/AboutMe";
+import { Home } from "./pages/Home/Home";
+import { Experience } from "./pages/Experience/Experience";
+import { Contact } from "./pages/Contact/Contact";
 
 function App() {
   return (
