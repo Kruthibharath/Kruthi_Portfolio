e9668ff138f4518817af97e938dd5050ad455b57
Kruthibharath <kruthi.bharath@gmail.com>
2024-02-08 17:03:22 +0000

    refactor: added strictMode

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
@@ -5,4 +6,8 @@ import "./index.css";
 // import "@fontsource/outfit";
 // import "@fontsource/roboto";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
