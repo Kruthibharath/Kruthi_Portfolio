d0b750ffdb88a1d9c9cee0a63a266ffb411e5d55
Kruthibharath <kruthi.bharath@gmail.com>
2024-02-08 13:57:55 +0000

    feat:added redirection, installed gh-pages to host the app on Github Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import { BrowserRouter } from "react-router-dom";
-import { Router } from "./components/Router/Router";
+import { HashRouter } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import { AboutMe } from "./pages/AboutMe/AboutMe";
+import { Home } from "./pages/Home/Home";
+import { Experience } from "./pages/Experience/Experience";
+import { Contact } from "./pages/Contact/Contact";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
+      <HashRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="aboutMe" element={<AboutMe />} />
+          <Route path="experience" element={<Experience />} />
+          <Route path="contact" element={<Contact />} />
+        </Routes>
+      </HashRouter>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
@@ -6,8 +5,4 @@ import "./index.css";
 import "@fontsource/outfit";
 import "@fontsource/roboto";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
