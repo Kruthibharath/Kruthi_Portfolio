7c80bbf453f9a996174eec96932b808d49231122
Kruthibharath <kruthi.bharath@gmail.com>
2024-02-08 16:22:27 +0000

    chore:deleted unwanted files along with build output path for hosting

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-import "@fontsource/outfit";
-import "@fontsource/roboto";
+// import "@fontsource/outfit";
+// import "@fontsource/roboto";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+//import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/Kruthi_Portfolio/",
   plugins: [react()],
+  build: {
+    outDir: "dist", // Change 'dist' to your desired output directory
+  },
 });
