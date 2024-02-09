3327ad9c4d85dde191599f3530b8a7fbcc1a8ee3
Kruthibharath <kruthi.bharath@gmail.com>
2024-02-09 10:24:14 +0000

    feat:added content for the experience page

--- src/pages/Experience/Experience.jsx
+++ src/pages/Experience/Experience.jsx
@@ -1,3 +1,56 @@
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import { experienceData } from "./experience_data";
+import { MdWork } from "react-icons/md";
+import "./Experience.scss";
 export const Experience = () => {
-  return <>Experience</>;
+  return (
+    <>
+      <section className="workExperience">
+        <div className="timeline">
+          <div className="timeline_experience">
+            <h3 className="timeline_experience_header">
+              Experience & Education
+            </h3>
+            <VerticalTimeline layout={"1-column"} lineColor="var(--primary)">
+              {experienceData.experience.map((item, i) => (
+                <VerticalTimelineElement
+                  key={i}
+                  className="timeline_experience_work"
+                  contentStyle={{
+                    background: "none",
+                    border: "2px solid var(--primary)",
+                    boxShadow: "2px 2px 0 var(--section-heading)",
+                  }}
+                  iconStyle={{ background: "var(--primary)", color: "#fff" }}
+                  icon={<MdWork />}
+                >
+                  <div className="work_content">
+                    <div className="work_content_header">
+                      <h4>{item.title}</h4>
+                      <h5>{item.date}</h5>
+                    </div>
+                    <h5>{item.Company}</h5>
+                    <p className="work_content_description">
+                      {item.description}
+                    </p>
+                    <ul className="work_content_tech">
+                      {item.TechStack.map((tech, index) => (
+                        <li key={index} className="work_content_tech_list">
+                          {tech}
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </VerticalTimelineElement>
+              ))}
+            </VerticalTimeline>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
---
+++ src/pages/Experience/experience_data.js
@@ -0,0 +1,105 @@
+export const experienceData = {
+  experience: [
+    {
+      title: "React, Typescript, TDD",
+      Company: "TechReturners",
+      date: "Oct 2023 - Jan 2024",
+      description:
+        "Upskilled in Front-end technologies by pursuing technical training encompassing the modules - Advanced JavaScript, Typescript, Semantic HTML and ARIA, MVCs, APIs, Responsive Design, React Hooks, Fetching Data in React, React Routers, TDD",
+      TechStack: [
+        "HTML5",
+        "CSS3",
+        "Tailwind",
+        "Javascript(ES6)",
+        "TypeScript",
+        "React",
+        "TDD",
+      ],
+    },
+
+    {
+      title: "Front End Web Developer(Contract)",
+      Company: "JPMorgan & Chase",
+      date: "Sep 2022 - Aug 2023",
+      description:
+        "Collaborated with Corporate Event Planners, LOBs , Developers across global teams to build, configure and maintain Corporate Event Management Email templates, Websites and Registration Forms",
+      TechStack: [
+        "HTML5",
+        "CSS3",
+        "Javascript",
+        "Email templates",
+        "Branding",
+        "Bootstrap",
+        "SASS",
+      ],
+    },
+    {
+      title: "Front End Web Developer",
+      Company: "esure",
+      date: "Jan 2018 - Sep 2022",
+      description:
+        "Developed more than 30 Optimization A/B tests per year and increased user engagement across direct and price comparison websites on different brands. Created reusable React components for efficient Software development experiments.",
+      TechStack: [
+        "HTML5",
+        "CSS3",
+        "Javascript",
+        "jQuery",
+        "React",
+        "Email templates",
+        "Optimizely",
+        "A/B testing",
+        "CRO",
+      ],
+    },
+    {
+      title: "Web Developer(Contract)",
+      Company: "Nimbe Limited",
+      date: "Jun 2017 - Nov 2017",
+      description:
+        "Contributed to building a company intranet portal for client, enabling employees, suppliers, and clients to participate and collaborate on business processes.",
+      TechStack: [
+        "HTML5",
+        "CSS3",
+        "SASS",
+        "Bootstrap",
+        "jQuery",
+        "Ajax",
+        "JSON",
+        "API calls",
+        "Backend integration",
+      ],
+    },
+    {
+      title: "JavaScript Developer(Contract)",
+      Company: "Trimble",
+      date: "Apr 2014 - Sep 2014",
+      description:
+        "Contributed to the development ‘Manhattan Space Scheduling’ a web-based on-line room booking system, used extensively by corporate clients to optimize the management of conference rooms, workspaces, and associated services",
+      TechStack: [
+        "HTML5",
+        "CSS3",
+        "Javascript",
+        "Cross-browser",
+        "Cross-device",
+        "Responsive design",
+      ],
+    },
+  ],
+  education: [
+    {
+      title: "Bachelor's degree",
+      field: "Computer Science",
+      Year: "2006-2010",
+    },
+    {
+      title: "Optimizely Certificate",
+      field: "Optimizely Web Developer",
+      Year: "2021-2026",
+    },
+    {
+      title: "Certificate",
+      field: "React Developer",
+      Year: "2022",
+    },
+  ],
+};
